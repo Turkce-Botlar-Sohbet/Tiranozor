a8bd0bc7b83480175005a692df5167d231d393d1
erkutof <52276842+erkutof@users.noreply.github.com>
2022-08-04 14:12:04 +0300

    Update ytdlp_button.py

--- plugins/ytdlp_button.py
+++ plugins/ytdlp_button.py
@@ -201,6 +201,9 @@ async def yt_dlp_call_back(bot, update):
     if HTTP_PROXY != "":
         command_to_exec.append("--proxy")
         command_to_exec.append(HTTP_PROXY)
+    if "dbx.molystream" in yt_dlp_url:
+        command_to_exec.append("--referer")
+        command_to_exec.append("https://dbx.molystream.org/")
     if ".cloud" in yt_dlp_url:
         command_to_exec.append("--referer")
         command_to_exec.append("https://vidmoly.to/")
